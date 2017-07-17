2041baf77610c3e8922613286a9f5456386bfc36
Jolrael4 <millywhite94@gmail.com>
2017-07-16 20:58:07 -0500

    html interaction added

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # PopPin
 
-Code started as Dukehammer's glowingStatic. I added the glitter function and limited the color during the disco function.
-
 The basic program pops a random number of pins in the same base color. 
-*Disco will start with and pop random colors. 
-*Glitter will be similar to disco, but will only pop the random color once the pin has faded to black.
+..*Disco will pop a random number of random colors. 
+..*Glitter is similar to disco, but will only pop the random color once the pin has faded to black.
+
+Code started as Dukehammer's glowingStatic. I added the following
+..*The glitter function 
+..*Limited the random color choice during the disco function.
+..*The HTML and CSS files
--- popPin.js
+++ popPin.js
@@ -3,40 +3,34 @@
 **********************/
 var particles = [];
 var numParticles = 2500;
-var radius = 10;
+var counter = 0;
+var step = 1;
+var W = 800; 
+var H = 800;
+var radius = 8;
 var prob = .2;
-var popFrequency = 20;
+var popFrequency = 22;
 var disco = false;
-var glitter = true;
-// var r = 255; var g = 255; var b = 255; // White
-// var r = 255; var g = 0; var b = 170; // Pink
-// var r = 220; var g = 0; var b = 0; // Red
-// var r = 255; var g = 125; var b = 0;   // Orange
-// var r = 255; var g = 255; var b = 0; // Yellow
-var r = 0; var g = 200; var b = 75; // Green
-// var r = 0; var g = 100; var b = 255; // Blue
-// var r = 170; var g = 0; var b = 170; // Purple
-// var r = 140; var g = 70; var b = 20; // Brown
-
+var glitter = false;
+var r = 0; var g = 200; var b = 75; //Green 6
 /*********************/
 
 // Get HTML elements
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var W = 1500; var H = 600;
+var colorSlide = document.getElementById("colorSlide");
+var probSlide = document.getElementById("probSlide");
+var freqSlide = document.getElementById("freqSlide");
 canvas.width = W;
 canvas.height = H;
 
 var running;
 
 // Create a number of particles and distribute their locations
-createParticles(numParticles);
+createParticles();
 
 // Run main every 1 ms
 running = setInterval(main, 1);
-
-var counter = 0;
-var step = 1;
 
 function main() {
 	// Increment counter between 0 and popFrequency
@@ -74,7 +68,7 @@ function Particle(x, y) {
 }
 
 // Create particles and assign x,y position
-function createParticles(numParticles) {
+function createParticles() {
 	rows = Math.sqrt(numParticles);
 	for (var i = 0; i < numParticles; i++) {
 		var x = W / (rows * i)
@@ -104,10 +98,7 @@ function popParticles() {
 	for (var i = 0; i < particles.length; i++) {
 		if (prob > Math.random()) {
 			if (disco) {
-        randomColor(particles[i]);
-				/*particles[i].r = Math.floor(Math.random() * 255);
-				particles[i].g = Math.floor(Math.random() * 255);
-				particles[i].b = Math.floor(Math.random() * 255);*/
+        		randomColor(particles[i]);
 			} else{
 				 particles[i].r = r;
 				 particles[i].g = g;
@@ -150,4 +141,62 @@ Particle.prototype.draw = function (ctx) {
 	ctx.beginPath();
 	ctx.arc(this.x, this.y, radius, 0, 2 * Math.PI, false);
 	ctx.fill();
+}
+
+
+//HTML helper functions
+//prob .1-.9
+function changeProb(value){
+	prob = value;
+}
+
+//freq 2-42 by 5
+function changeFreq(value){
+	popFrequency = value;
+}
+
+function pickBasic(){
+	disco = false;
+	glitter = false;
+	colorSlide.style.display = 'block'; 
+	probSlide.style.display = 'block';
+	freqSlide.style.display = 'block';
+}
+
+function pickDisco(){
+	disco = true;
+	glitter = false;
+	colorSlide.style.display = 'none'; 
+	probSlide.style.display = 'block';
+	freqSlide.style.display = 'block';
+}
+
+function pickGlitter(){
+	glitter = true;
+	disco = false;
+	colorSlide.style.display = 'none'; 
+	probSlide.style.display = 'block';
+	freqSlide.style.display = 'block';
+}
+
+function changeColor(value){
+	if (value == 1) {
+		r = 255;  g = 255;  b = 255; // White 1
+	} else{ if (value == 2) {
+		r = 255;  g = 0;  b = 170; //Pink 2
+	} else{ if (value == 3) {
+		r = 220;  g = 0;  b = 0; //Red 3
+	} else{ if (value == 4) {
+		r = 255;  g = 125;  b = 0; // Orange 4
+	} else{ if (value == 5) {
+		r = 255;  g = 255;  b = 0; //Yellow 5
+	} else{ if (value == 6) {
+		r = 0;  g = 200;  b = 75; //Green 6
+	} else{ if (value == 7) {
+		r = 0;  g = 100;  b = 255; // Blue 7
+	} else{ if (value == 8) {
+		r = 170;  g = 0;  b = 170; //Purple 8
+	} else{
+		r = 255; g = 170; b = 85; //Brown 9
+	};};};};};};};};
 }
